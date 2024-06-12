ce17b1df1f1684e2bc2c4ddc62d0c7355a3a8364
Grigory <weissman1979@gmail.com>
2024-06-12 22:28:00 +0400

    commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import CreatePost from './components/CreatePost/CreatePost';
+import EditPost from './components/EditPost/EditPost';
+import MainPage from './components/MainPage/MainPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<MainPage />} />
+          <Route path='/CreatePost' element={<CreatePost />} />
+          <Route path='/EditPost' element={<EditPost />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
